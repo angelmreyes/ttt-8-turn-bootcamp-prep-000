f56dc9439b3336f2c3ead09716c6918c3633de92
loading-torrent-9632 <angelrey@buffalo.edu>
2019-04-18 13:07:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,18 @@
-def turn
-  # ask for input
-  # get input
-  # convert input to index
-  # if index is valid
-  #   make the move for index
-  #   show the board
-  # else
-  #   ask for input again until you get a valid input
-  # end
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, position, char = "X")
+  board[position] = char
 end
 
 # code your #valid_move? method here
@@ -22,4 +27,16 @@ def position_taken?(board, index)
   else
     true
   end
+end
+
+def turn
+  # ask for input
+  # get input
+  # convert input to index
+  # if index is valid
+  #   make the move for index
+  #   show the board
+  # else
+  #   ask for input again until you get a valid input
+  # end
 end
